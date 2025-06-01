d5dadd65db8b93485388ed3fbaf049d4cc0f304e
AvosahMillie <milicentavosa@gmail.com>
2025-06-01 17:33:44 -0400

    Add initial setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#  Create directory structure 
+mkdir -p data/raw_fastq
+mkdir -p data/genome
+mkdir -p docs
+mkdir -p results/fastqc_raw
+mkdir -p results/fastqc_trimmed
+mkdir -p results/trimmed_fastq
+mkdir -p results/sam
+mkdir -p results/bam
+mkdir -p results/bcf
+mkdir -p results/vcf
+mkdir -p scripts
